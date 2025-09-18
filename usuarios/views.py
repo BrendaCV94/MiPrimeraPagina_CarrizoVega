2f230cf39300893d27c6d54f8857cd83e8412f6a
Brenda <brendacarrizovega@gmail.com>
2025-09-17 22:13:20 -0300

    Agregado campo imagen a Libro y avatar en perfil de usuario

--- usuarios/views.py
+++ usuarios/views.py
@@ -7,13 +7,15 @@ from .forms import RegistroUsuarioForm, EditarPerfilForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from .forms import EditarContraseñaForm
+from usuarios.models import DatosExtra
 
-# Registro de usuario (NO requiere login)
 def registrar_usuario(request):
     if request.method == "POST":
         form = RegistroUsuarioForm(request.POST)
         if form.is_valid():
             usuario = form.save()
+            # Crear perfil extra vacío
+            DatosExtra.objects.create(user=usuario)
             login(request, usuario)  # lo loguea directo después de registrarse
             messages.success(request, "Registro exitoso.")
             return redirect("inicio")
@@ -23,7 +25,7 @@ def registrar_usuario(request):
         form = RegistroUsuarioForm()
     return render(request, "usuarios/registro.html", {"form": form})
 
-# Iniciar sesión (NO requiere login previo)
+
 def iniciar_sesion(request):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
@@ -38,7 +40,7 @@ def iniciar_sesion(request):
         form = AuthenticationForm()
     return render(request, "usuarios/iniciar_sesion.html", {"form": form})
 
-# Cerrar sesión (puede hacerlo cualquiera logueado)
+
 def cerrar_sesion(request):
     logout(request)
     messages.info(request, "Sesión cerrada.")
@@ -48,8 +50,15 @@ def cerrar_sesion(request):
 
 @login_required(login_url='iniciar_sesion')
 def perfil(request):
+    datos_extra, creado = DatosExtra.objects.get_or_create(user=request.user)
+
     if request.method == "POST":
         form = EditarPerfilForm(request.POST, instance=request.user)
+        avatar_form = None
+        if request.FILES.get("avatar"):  # Si subió un archivo
+            datos_extra.avatar = request.FILES["avatar"]
+            datos_extra.save()
+
         if form.is_valid():
             form.save()
             messages.success(request, "Perfil actualizado correctamente.")
@@ -57,7 +66,10 @@ def perfil(request):
     else:
         form = EditarPerfilForm(instance=request.user)
 
-    return render(request, "usuarios/perfil.html", {"form": form})
+    return render(request, "usuarios/perfil.html", {
+        "form": form,
+        "datos_extra": datos_extra,
+    })
 
 class EditarContraseñaView(PasswordChangeView):
     template_name = "usuarios/editar_contraseña.html"
